b4c48378cf7533eac135fe9cc65d35ba93fd935a
1 <65522530+quantrananh2304@users.noreply.github.com>
2023-11-14 14:03:28 +0700

    update api method from get to put

--- src/api/ads.ts
+++ src/api/ads.ts
@@ -1,4 +1,4 @@
-import { sendGet } from "~/utils/axios";
+import { sendGet, sendPut } from "~/utils/axios";
 
-export const getAdsList = () => sendGet('/ads/get-list-ads');
-export const adsClick = (adsId: any) => sendGet(`/ads/${adsId}/view-ads`);
+export const getAdsList = () => sendGet("/ads/get-list-ads");
+export const adsClick = (adsId: any) => sendPut(`/ads/${adsId}/view-ads`);
